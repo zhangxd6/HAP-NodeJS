fd3ba15590c7272528cf56dfb4e26f73efa4e17d
zhangxd6 <zhangxd6@gmail.com>
2016-07-17 18:09:00 -0500

    add iothub

--- accessories/temperatureSensorReader.js
+++ accessories/temperatureSensorReader.js
@@ -17,6 +17,17 @@ function Reader(){
         parser: serialport.parsers.readline("\n")
     });
 
+    var clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
+    var Message = require('azure-iot-device').Message;
+
+    var connectionString = process.env.ATTIC_CONNECTIONSTRING;
+    var connectionString2 = process.env.GARAGE_CONNECTIONSTRING;
+
+    var clients = {
+        garage:clientFromConnectionString(connectionString2),
+        attic:clientFromConnectionString(connectionString)
+    };
+
 
     function printResultFor(op) {
     return function printResult(err, res) {
@@ -25,19 +36,59 @@ function Reader(){
     };
     }
 
-    sp.on('data',function(input) {
-        debug('data:'+ input);
+    // sp.on('data',function(input) {
+    //     debug('data:'+ input);
+    //     try{
+    //         var data = JSON.parse(input);
+    //         if(data){
+    //             data["timestamp"] = new Date().toISOString();    
+    //             self.emit('event',data);      
+    //         }
+    //     }
+    //     catch(err){
+        
+    //     }
+    //})
+
+    var connectCallback = function (err,c) {
+    if (err) {
+        debug('Could not connect: ' + err);
+    } else {
+        debug('Client connected');
+
+        // Create a message and send it to the IoT Hub every second
+        sp.on('data', function(input) {
+        //console.log(input);
         try{
             var data = JSON.parse(input);
             if(data){
-                data["timestamp"] = new Date().toISOString();    
-                self.emit('event',data);      
+                if(c===data.deviceId &&clients[data.deviceId]){
+                    data["timestamp"] = new Date().toISOString();
+                    self.emit('event',data);  
+                    //console.log(data);
+                    var message = new Message(JSON.stringify(data));
+                    console.log("Sending message: " + message.getData());
+                    clients[data.deviceId].sendEvent(message, printResultFor('send'));
+               }
             }
         }
         catch(err){
         
         }
-    })
+        });
+    }
+    };
+
+    clients['garage'].open(function(err) {
+        connectCallback(err,'garage');
+    });
+    clients['attic'].open(function(err) {
+        connectCallback(err,'attic');
+    });
 }
+
+
+
+
 inherits(Reader, EventEmitter);
 exports.reader = new Reader();
